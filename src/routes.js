9ecb1c68e1dd540ad3d2b7cc19524379b041c30d
Fahrezi <mohfahrezi93@gmail.com>
2024-11-09 23:57:17 +0700

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -1,50 +1,10 @@
-const editNoteByIdHandler = (request, h) => {
-    const { id } = request.params;
-   
-    const { title, tags, body } = request.payload;
-    const updatedAt = new Date().toISOString();
-   
-    const index = notes.findIndex((note) => note.id === id);
-   
-    if (index !== -1) {
-      notes[index] = {
-        ...notes[index],
-        title,
-        tags,
-        body,
-        updatedAt,
-      };
-   
-      const response = h.response({
-        status: 'success',
-        message: 'Catatan berhasil diperbarui',
-      });
-      response.code(200);
-      return response;
-    }
-   
-    const response = h.response({
-      status: 'fail',
-      message: 'Gagal memperbarui catatan. Id tidak ditemukan',
-    });
-    response.code(404);
-    return response;
-  };
-   
-  module.exports = {
-    addNoteHandler,
-    getAllNotesHandler,
-    getNoteByIdHandler,
-    editNoteByIdHandler,
-  };
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
 
-const { addNoteHandler, getAllNotesHandler } = require('./handler');
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    handler: () => {},
+    handler: addNoteHandler,
   },
   {
     method: 'GET',
@@ -61,8 +21,11 @@ const routes = [
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
   },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
-
-module.exports = { addNoteHandler, getAllNotesHandler };
